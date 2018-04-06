e0794664e319d506783de72fbe10fe12282fa610
Gary Tuttle <gtuttle@iastate.edu>
2018-04-05 23:27:44 -0500

    Update quiz24.c

--- quiz24.c
+++ quiz24.c
@@ -19,7 +19,7 @@ int main( void ) {
    //intitalze the members of the struct with random values
    for( i = 0; i < 10; i++ ){
       aBunchOfGuys[i].age = rand()%46 + 20;        //Age 20 to 65
-      aBunchOfGuys[i].height = rand()%21 + 50;     //5 ft 0 in to 6 ft 8 in
+      aBunchOfGuys[i].height = rand()%21 + 60;     //5 ft 0 in to 6 ft 8 in
       aBunchOfGuys[i].weight = rand()%181 + 120;   //120 lb to 300 lb
    }
    
