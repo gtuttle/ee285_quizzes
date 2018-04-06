806db6c8734650903b254875977fa2c9cbf948ff
Gary Tuttle <gtuttle@iastate.edu>
2018-04-05 23:53:55 -0500

    Create quiz22.c

---
+++ quiz22.c
@@ -0,0 +1,49 @@
+// EE 285 - quiz 22
+
+#include <stdio.h>
+
+int countTheOs( char[] );    // or char*
+
+int main( void ) {
+   
+   char str[] = "Now is the time for all good men to come to the aid of their country.";
+   int numberOfOs;
+   
+   numberOfOs = countTheOs( str );  //Remember, str is pointer to the first element of the string.
+   
+   printf( "The number of o's is: %d.\n\n", numberOfOs );
+   
+   return 0;
+}
+
+int countTheOs( char s[] ){
+   
+   int i = 0, count = 0;
+   
+   while( s[i] != '\0' ){       //step through the string until you hit the "0" at the end.
+      if( s[i] == 'o' ){        //if the string element happens to by a 'o', count it.
+         count++;
+      }
+      i++;
+   }
+   
+   return count;               //the return the final value of count
+}
+
+/*Output
+ 
+ The number of o's is: 9.
+ 
+ Program ended with exit code: 0
+ 
+ */
+
+/*Alternative for the while loop
+ 
+ while( s[i] != '\0' )
+   if( s[i++] == 'o' )
+      count++;
+*/
+
+
+
